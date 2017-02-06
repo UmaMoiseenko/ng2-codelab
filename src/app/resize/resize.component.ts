bcaf11d2613cabd47cbe7f11721d681c37b9fa61
UmaMoiseenko <uma@KRG-F29F-Uma.local>
2017-02-06 00:11:58 -0500

    implementing vertical resize

--- src/app/resize/resize.component.ts
+++ src/app/resize/resize.component.ts
@@ -1,4 +1,20 @@
-import {Component, ElementRef, HostListener} from '@angular/core';
+/*
+Resize component - used to resize passed components horizontally or vertically.
+Horizontal resize is the default.
+Inputs:
+direction - options: 'vertical', 'horizontal'. Default 'horizontal'
+collapsed - options: true / false. Default false. Used for components that have expanded and collapsed state
+disabled - options true / false. Default false. Useful when displaying sets of elements where only some of the have resize property.
+
+*/
+
+import {
+  Component,
+  ElementRef,
+  Input,
+  HostListener,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-resize',
@@ -6,45 +22,116 @@ import {Component, ElementRef, HostListener} from '@angular/core';
   styleUrls: ['./resize.component.css']
 })
 export class ResizeComponent {
-  private initOffsetX;
-  private initWidth;
-  private isMouseDown: boolean;
-  private width;
-  private minWidth = 400;
+  @Input() direction;
+  @Input() collapsed;
+  @Input() disabled;
+  @ViewChild('content') content: ElementRef;
 
-  constructor(private elementRef: ElementRef) {
-  }
+  private heightModel;
+  private isMouseDown: boolean;
+  private MIN_WIDH = 400;
+  private MIN_HEIGHT = 100;
+  private keepBelowContent = false;
+
+  private initOffset;
+  private initSize;
+  private minSize;
+  private size;
+
+  constructor(private elementRef: ElementRef) {}
+
+  private contentHeight;
 
   ngOnInit() {
-    this.width = this.elementRef.nativeElement.clientWidth;
+    if (this.disabled) { return; }
+
+    if (this.direction === 'vertical') {
+      this.minSize = this.MIN_HEIGHT;
+      this.size = Math.max(this.minSize, this.elementRef.nativeElement.clientHeight);
+
+    } else {
+      this.minSize = this.MIN_WIDH;
+      this.size = this.elementRef.nativeElement.clientWidth;
+    }
   }
 
-  calcWidth(x) {
-    return this.initWidth + x - this.initOffsetX
+  isVertical() {
+    return this.direction === 'vertical';
+  }
+
+  getWidth() {
+    if (this.isVertical()) { return; }
+    return this.size
+  }
+
+  getHeight() {
+    if (this.disabled) { return; }
+
+    if (this.collapsed) {
+      return 0;
+    } else {
+      return this.size;
+    }
+  }
+
+  getMinHeight() {
+    this.contentHeight = this.content.nativeElement.clientHeight;
+
+    if (this.contentHeight <= this.size) {
+      this.keepBelowContent = true;
+
+    } else if (this.keepBelowContent === true && this.contentHeight > this.size) {
+      this.size = this.contentHeight;
+    }
+      return Math.min(this.size, this.contentHeight);
+  }
+
+  calcSize(currentOffset) {
+    return this.initSize + currentOffset - this.initOffset
   }
 
   @HostListener('mousemove', ['$event'])
   onMouseMove(e) {
-    if (!this.isMouseDown) {
-      return;
-    }
+    if (!this.isMouseDown || this.disabled) { return; }
+    this.keepBelowContent = false;
     e.preventDefault();
 
-    this.width = Math.max(this.minWidth, this.calcWidth(e.clientX));
+    let offset;
+    if (!this.isVertical()) {
+      offset = e.clientX;
+    } else {
+      offset = e.clientY;
+    }
+
+    this.size = Math.max(this.minSize, this.calcSize(offset));
   }
 
   @HostListener('mousedown', ['$event'])
   onMouseDown(e) {
+    if (this.disabled) { return; }
+
     if (e.target.className && (e.target.className.includes('spacer') || e.target.className.includes('handle') )) {
+      e.stopPropagation();
       this.isMouseDown = true;
-      this.initOffsetX = e.clientX;
-      this.initWidth = this.width
+      this.initSize = this.size;
+
+      if (!this.isVertical()) {
+        this.initOffset = e.clientX;
+      } else {
+        this.initOffset = e.clientY;
+      }
     }
   }
 
   @HostListener('mouseup')
-  @HostListener('mouseleave')
   mouseStateReset() {
     this.isMouseDown = false;
+  }
+
+  @HostListener('mouseleave')
+  mouseOptionalReset() {
+    if (!this.isVertical()) {
+      this.isMouseDown = false;
+    }
   }
 }
