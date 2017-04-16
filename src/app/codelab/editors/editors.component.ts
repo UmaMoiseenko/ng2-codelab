ba87e030aa2cad97943b2e4d819b69cf54afb401
UmaMoiseenko <uma@KRG-F29F-Uma.local>
2017-04-16 19:00:58 -0400

    #65 adding multi tabs to editors

--- src/app/codelab/editors/editors.component.ts
+++ src/app/codelab/editors/editors.component.ts
@@ -8,27 +8,45 @@ import {AppConfigService} from '../../app-config.service';
 @Component({
   selector: 'app-editors',
   templateUrl: './editors.component.html',
-  styleUrls: ['./editors.component.css']
+  styleUrls: ['./editors.component.scss']
 })
 export class EditorsComponent {
   @Input() public files: Array<any>;
   @ViewChildren(EditorComponent) children: QueryList<EditorComponent>;
   private debug: boolean;
+  private currentFile;
 
   get visibleFiles() {
     return this.files.filter(file => !file.hidden);
-  }
-
-  onCodeChange(change) {
-    this.state.updateCode(change);
   }
 
   constructor(private  state: StateService, appConfig: AppConfigService) {
     this.debug = appConfig.config.debug;
   }
 
-  toggleFile(file: FileConfig) {
-    this.state.toggleFile(file);
+  onCodeChange(change) {
+    this.state.updateCode(change);
+  }
+
+  ngOnInit() {
+    this.currentFile = this.visibleFiles[0]
+  }
+
+  getFileName(file) {
+    return file.path.replace(/^.*[\\\/]/, '');
+  }
+
+  getOpenedFile() {
+    return this.currentFile;
+  }
+
+  isOpenFile(file) {
+    if (!file || !this.currentFile) { return; }
+    return file.path == this.currentFile.path;
+  }
+
+  showFile(file) {
+    this.currentFile = file;
   }
 
   loadSolution(file: FileConfig) {
